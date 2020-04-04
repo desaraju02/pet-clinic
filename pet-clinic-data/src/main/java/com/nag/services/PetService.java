2460c00be2dfb1849df9bb4abf5cb6f2336b56dc
Desaraju <desaraju02@gmail.com>
2020-04-05 01:02:27 +0530

    Basic Interface Services

---
+++ pet-clinic-data/src/main/java/com/nag/services/PetService.java
@@ -0,0 +1,15 @@
+package com.nag.services;
+
+
+import com.nag.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/nag/services/VetService.java
@@ -0,0 +1,12 @@
+package com.nag.services;
+
+import com.nag.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
