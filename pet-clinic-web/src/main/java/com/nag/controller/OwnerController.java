23886c589af240a5f7c4664a5668bc164e91cb1a
Desaraju <desaraju02@gmail.com>
2020-08-01 07:56:21 +0530

    Adding Owners files

--- pet-clinic-web/src/main/java/com/nag/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/nag/controller/OwnerController.java
@@ -1,11 +1,15 @@
 package com.nag.controller;
 
 import com.nag.model.Owner;
+import com.nag.model.Pet;
 import com.nag.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -29,4 +33,17 @@ public class OwnerController {
     {
         return "notimplemented";
     }
+
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+//        for (Pet pet : owner.getPets()) {
+//            pet.setVisitsInternal(visits.findByPetId(pet.getId()));
+//        }
+        mav.addObject(owner);
+        return mav;
+    }
+
 }
